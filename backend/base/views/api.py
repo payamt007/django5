fcc9dea2fc0ef72e87b75ceabc535ab5998b3dfc
Payam <payamdvlp007@gmail.com>
2023-07-27 11:02:06 +0330

    fix : resolved dependecy errors

--- backend/base/views/api.py
+++ backend/base/views/api.py
@@ -2,7 +2,13 @@ import feedparser
 from rest_framework import viewsets
 from rest_framework import mixins
 from base.models import Post, Feed
-from base.serializers import PostSerializer, FeedSerializer, FilterSerializer, ForceRefreshSerializer, UpdatePostSerializer
+from base.serializers import (
+    PostSerializer,
+    FeedSerializer,
+    FilterSerializer,
+    ForceRefreshSerializer,
+    UpdatePostSerializer,
+)
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -19,24 +25,32 @@ class PostViewSet(mixins.UpdateModelMixin, viewsets.GenericViewSet):
     serializer_class = UpdatePostSerializer
 
 
-class FeedViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+class FeedViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
 
     """
-    Viewset for handling creating, listing and updating (unfollowing, following) rss feeds
+    Viewset for handling creating, listing and
+    updating (unfollowing, following) rss feeds
     """
 
     queryset = Feed.objects.all()
     serializer_class = FeedSerializer
 
-    @extend_schema(description='Insert a new Feed')
+    @extend_schema(description="Insert a new Feed")
     def create(self, request, *args, **kwargs) -> Response:
         return super().create(request, *args, **kwargs)
 
-    @extend_schema(description='Retrieve a list of Feeds')
+    @extend_schema(description="Retrieve a list of Feeds")
     def list(self, request, *args, **kwargs) -> Response:
         return super().list(request, *args, **kwargs)
 
-    @extend_schema(description='Update a Feed , followed to true or false , Or maybe change title or url')
+    @extend_schema(
+        description="Update a Feed , followed to true or false , Or maybe change title or url"
+    )
     def update(self, request, *args, **kwargs) -> Response:
         return super().update(request, *args, **kwargs)
 
@@ -55,8 +69,9 @@ class PostFilterAPIView(APIView):
         serializer = FilterSerializer(data=query_params)
         serializer.is_valid(raise_exception=True)
         order_by_param = serializer.validated_data.pop("order_by")
-        filterd_results = Post.objects.filter(
-            **serializer.validated_data).order_by(order_by_param)
+        filterd_results = Post.objects.filter(**serializer.validated_data).order_by(
+            order_by_param
+        )
         # filterd_results = Post.objects.order_by(order_by_param[0])
         result_serializer = PostSerializer(filterd_results, many=True)
 
@@ -75,7 +90,7 @@ class ForceRefreshAPIview(APIView):
     def post(self, request) -> Response:
         serializer = ForceRefreshSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        id = serializer.validated_data['id']
+        id = serializer.validated_data["id"]
         feed = Feed.objects.get(id=id)
 
         try:
@@ -87,7 +102,7 @@ class ForceRefreshAPIview(APIView):
                     new_post.title = item.title
                     new_post.link = item.link
                     new_post.description = item.description
-                    if hasattr(item, 'pubDate'):
+                    if hasattr(item, "pubDate"):
                         new_post.pubDate = item.pubDate
                     new_post.feed = feed
                     new_post.save()
@@ -95,7 +110,14 @@ class ForceRefreshAPIview(APIView):
             feed.fails = 0
             feed.stopped = False
             feed.save()
-            return Response(data={"message": "Feed refresh Success! Feed returned to normal scrapping schedule!"},
-                            status=status.HTTP_200_OK)
+            return Response(
+                data={
+                    "message": "Feed refresh Success! Feed returned to normal scrapping schedule!"
+                },
+                status=status.HTTP_200_OK,
+            )
         except Exception:
-            return Response(data={"message": "Feed refresh Faild! Sorry!"}, status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(
+                data={"message": "Feed refresh Faild! Sorry!"},
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
