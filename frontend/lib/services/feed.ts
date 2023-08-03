5aaf79b0d24833fdbc9bb4e98527518b7537e04c
Payam <payamdvlp007@gmail.com>
2023-08-03 18:14:08 +0430

    feat : added mutation

--- frontend/lib/services/feed.ts
+++ frontend/lib/services/feed.ts
@@ -1,16 +1,54 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import {paginatedFeedType} from '../types/paginated'
+import { paginatedFeedType } from '../types/paginated'
+import { feedType } from '../types/feeds'
 
 // Define a service using a base URL and expected endpoints
 export const feedsApi = createApi({
   reducerPath: 'feedApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api' }),
-  endpoints: (builder) => ({
-    getAllFeeds: builder.query<paginatedFeedType, void>({
+  endpoints: (build) => ({
+    getAllFeeds: build.query<paginatedFeedType, void>({
       query: () => `feeds`,
     }),
-  }),
+    updatePost: build.mutation<void, Pick<feedType, 'title' | 'link'>>({
+      // note: an optional `queryFn` may be used in place of `query`
+      query: ({ ...patch }) => ({
+        url: `feeds`,
+        method: 'POST',
+        body: patch,
+      }),
+      // Pick out data and prevent nested properties in a hook or selector
+      transformResponse: (response: { data: Post }, meta, arg) => response.data,
+      // Pick out errors and prevent nested properties in a hook or selector
+      transformErrorResponse: (
+        response: { status: string | number },
+        meta,
+        arg
+      ) => response.status,
+      invalidatesTags: ['feedType'],
+      // onQueryStarted is useful for optimistic updates
+      // The 2nd parameter is the destructured `MutationLifecycleApi`
+      async onQueryStarted(
+        arg,
+        { dispatch, getState, queryFulfilled, requestId, extra, getCacheEntry }
+      ) { },
+      // The 2nd parameter is the destructured `MutationCacheLifecycleApi`
+      async onCacheEntryAdded(
+        arg,
+        {
+          dispatch,
+          getState,
+          extra,
+          requestId,
+          cacheEntryRemoved,
+          cacheDataLoaded,
+          getCacheEntry,
+        }
+      ) { },
+    }),
+  })
 })
+
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
